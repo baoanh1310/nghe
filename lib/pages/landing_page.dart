3b28998316ad3305246314a69aa2e7f4ce2687b3
baoanh1310 <dataholicguy@gmail.com>
2022-07-27 15:14:51 +0700

    add assets and complete Welcome screen

---
+++ lib/pages/landing_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:nghe/values/app_assets.dart';
+import 'package:nghe/values/app_colors.dart';
+import 'package:nghe/values/app_styles.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primaryColor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Welcome to',
+                  style: AppStyles.h3,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      'Nghệ',
+                      style: AppStyles.h1.copyWith(
+                        color: AppColors.blackGrey,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: Text(
+                        'Quotes"',
+                        textAlign: TextAlign.right,
+                        style: AppStyles.h4.copyWith(
+                          height: 1.0
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 72),
+                child: RawMaterialButton(
+                  onPressed: () {},
+                  child: Image.asset(AppAssets.rightArrow),
+                  shape: CircleBorder(),
+                  fillColor: AppColors.lightBlue,
+                ),
+              ),
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
